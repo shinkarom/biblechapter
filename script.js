ca919c93bd14ddd575e71a39c91cee6451fe4418
shinkarom <shinkarom@gmail.com>
2020-04-29 16:09:21 +0300

    Add some verse modes; verse counts for some books; temporarily broken

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var mode = "cr";
 var lang = 0
 var book = 1
 var chapter = 1
+var verse = 1
 
 function randomInt(minn, maxn) {
 	return Math.floor(Math.random()*(maxn-minn))+minn;
@@ -11,35 +12,69 @@ function genBook(){
 	book = randomInt(0, chapters.length)+1;
 }
 
-function genChapterFromRandomBook(){
+function genChapterFromBook(){
 	genBook();
-	chapter = randomInt(0, chapters[book-1])+1;
+	chapter = randomInt(0, chapters[book-1].length)+1;
 }
 
-function genChapterProportionally(){
+function genChapter(){
 	var total = 0;
-	for(var i = 0;i<chapters.length;i++){ total += chapters[i];}
+	for(var i = 0;i<chapters.length;i++){ total += chapters[i].length;}
 	var r = randomInt(0, total);
 	for(var i = 0;i<chapters.length;i++ ) {
-		if(r < chapters[i]){
+		if(r < chapters[i].length){
 			book = i + 1;
 			chapter = r + 1;
 			break;
 		} else
-			r -= chapters[i];
+			r -= chapters[i].length;
 	}
+}
+
+function genVerse(){
+	
+}
+
+function genVerseFromBook(){
+	genBook();
+	var total = 0;
+	for(var i = 0;i<chapters[book-1].length;i++){ total += chapters[book-1][i];}
+	var r = randomInt(0, total);
+	for(var i = 0;i<chapters[book-1].length;i++ ) {
+		if(r < chapters[book-1][i]){
+			chapter = i + 1;
+			verse = r + 1;
+			break;
+		} else
+			r -= chapters[book-1][i];
+	}	
+}
+
+function genVerseFromChapter(){
+	genChapter();
+	var count = chapters[book-1][chapter-1];
+	verse = randomInt(0, count)+1;
 }
 
 function generate(){
 	switch(mode){
-		case "cr":
-			genChapterFromRandomBook();
+		case "cb":
+			genChapterFromBook();
 			break;
-		case "cp":
-			genChapterProportionally();
+		case "c":
+			genChapter();
 			break;
 		case "b":
 			genBook();
+			break;
+		case "vb":
+			genVerseFromBook();
+			break;
+		case "vc":
+			genVerseFromChapter();
+			break;
+		case "v":
+			genVerse();
 			break;
 		default:
 			break;
@@ -60,6 +95,10 @@ function onGenClick(){
 		link += "/";
 		link += chapter;		
 		n += " " + chapter;		
+		if((mode=="v")||(mode=="vb")||(mode=="vc")){
+			link += "#v=" + book + ":" + chapter + ":" + verse;
+			n += ":" + verse;
+		}
 	}
 	document.querySelector("#link").href = link;
 	document.querySelector("#link").innerText = n;
@@ -68,6 +107,7 @@ function onGenClick(){
 function onModeChange(){
 	var e = document.querySelector("#mode")
 	mode = e.options[e.selectedIndex].value;
+	onGenClick();
 }
 
 function onLangsChange() {
@@ -75,6 +115,7 @@ function onLangsChange() {
 	for(var i = 0; i<languages.length;i++){
 		if(languages[i].option==langs.options[langs.selectedIndex].value){
 			lang = i;
+			onGenClick();
 			return;
 		}
 	}
@@ -99,77 +140,76 @@ function bodyLoad(){
 	document.querySelector("#mode").addEventListener("change", onModeChange);
 	onLangsChange();
 	onModeChange();
-	onGenClick();
 }
 
 var defaultLang = "en"
 var chapters = [
-	50, //Genesis
-	40, //Exodus
-	27, //Leviticus
-	36, //Numbers
-	34, //Deuteronomy
-	24, //Joshua
-	21, //Judges
-	4, //Ruth
-	31, //1 Samuel
-	24, //2 Samuel
-	22, //1 Kings
-	25, //2 Kings
-	29, //1 Chronicles
-	36, //2 Chronicles
-	10, //Ezra
-	13, //Nehemiah
-	10, //Esther
-	42, //Job
-	150, //Psalms
-	31, //Proverbs
-	12, //Ecclesiastes
-	8, //Song of Solomon
-	66, //Isaiah
-	52, //Jeremiah
-	5, //Lamentations
-	48, //Ezekiel
-	12, //Daniel
-	14, //Hosea
-	3, //Joel
-	9, //Amos
-	1, //Obadiah
-	4, //Jonah
-	7, //Micah
-	3, //Nahum
-	3, //Habakkuk
-	3, //Zephaniah
-	2, //Haggai
-	14, //Zechariah
-	4, //Malachi
-	28, //Matthew
-	16, //Mark
-	24, //Luke
-	21, //John
-	28, //Acts
-	16, //Romans
-	16, //1 Corinthians
-	13, //2 Corinthians
-	6, //Galatians
-	6, //Ephesians
-	4, //Philippians
-	4, //Colossians
-	5, //1 Thessalonians
-	3, //2 Thessalonians
-	6, //1 Timothy
-	4, //2 Timothy
-	3, //Titus
-	1, //Philemon
-	13, //Hebrews
-	5, //James
-	5, //1 Peter
-	3, //2 Peter
-	5, //1 John
-	1, //2 John
-	1, //3 John
-	1, //Jude
-	22 //Revelation
+	[50], //Genesis
+	[40], //Exodus
+	[27], //Leviticus
+	[36], //Numbers
+	[34], //Deuteronomy
+	[24], //Joshua
+	[21], //Judges
+	[22, 23, 18, 22], //Ruth
+	[31], //1 Samuel
+	[24], //2 Samuel
+	[22], //1 Kings
+	[25], //2 Kings
+	[29], //1 Chronicles
+	[36], //2 Chronicles
+	[10], //Ezra
+	[13], //Nehemiah
+	[10], //Esther
+	[42], //Job
+	[150], //Psalms
+	[31], //Proverbs
+	[12], //Ecclesiastes
+	[17, 17, 11, 16, 16, 13, 13, 14], //Song of Solomon
+	[66], //Isaiah
+	[52], //Jeremiah
+	[22, 22, 66, 22, 22], //Lamentations
+	[48], //Ezekiel
+	[12], //Daniel
+	[14], //Hosea
+	[20, 32, 21], //Joel
+	[15, 16, 15, 13, 27, 14, 17, 14, 15], //Amos
+	[21], //Obadiah
+	[17, 10, 10, 11], //Jonah
+	[16, 13, 12, 13, 15, 16, 20], //Micah
+	[15, 13, 19], //Nahum
+	[17, 20, 19], //Habakkuk
+	[18, 15, 20], //Zephaniah
+	[15, 23], //Haggai
+	[14], //Zechariah
+	[14, 17, 18, 6], //Malachi
+	[28], //Matthew
+	[16], //Mark
+	[24], //Luke
+	[21], //John
+	[28], //Acts
+	[16], //Romans
+	[16], //1 Corinthians
+	[13], //2 Corinthians
+	[24, 21, 29, 31, 26, 18], //Galatians
+	[23, 22, 21, 31, 33, 24], //Ephesians
+	[30, 30, 21, 23], //Philippians
+	[29, 23, 25, 18], //Colossians
+	[10, 20, 13, 18, 28], //1 Thessalonians
+	[12, 17, 18], //2 Thessalonians
+	[20, 15, 16, 16, 25, 21], //1 Timothy
+	[18, 26, 17, 22], //2 Timothy
+	[16, 15, 15], //Titus
+	[25], //Philemon
+	[13], //Hebrews
+	[27, 26, 18, 17, 20], //James
+	[25, 25, 22, 19, 14], //1 Peter
+	[21, 22, 18], //2 Peter
+	[10, 29, 24, 21, 21], //1 John
+	[13], //2 John
+	[14], //3 John
+	[25], //Jude
+	[22] //Revelation
 	]
 var languages = [
 	{"lang": "English","option":"en",
