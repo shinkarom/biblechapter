30cdf76b91be15686e576e10ee445742c07169f9
shinkarom <shinkarom@gmail.com>
2020-04-29 21:24:04 +0300

    Add a few more books; temporarily broken

--- script.js
+++ script.js
@@ -18,10 +18,8 @@ function genChapterFromBook(){
 }
 
 function genChapter(){
-	var total = 0;
-	for(var i = 0;i<chapters.length;i++){ total += chapters[i].length;}
-	var r = randomInt(0, total);
-	for(var i = 0;i<chapters.length;i++ ) {
+	var r = randomInt(0, chapterTotal);
+	for(var i = 0;i<chapters.length;i++) {
 		if(r < chapters[i].length){
 			book = i + 1;
 			chapter = r + 1;
@@ -32,7 +30,19 @@ function genChapter(){
 }
 
 function genVerse(){
-	
+	var r = randomInt(0, verseTotal);
+	for(var i = 0;i < chapters.length;i++) {
+		for(var j = 0; j < chapters[i].length; j++) {
+			if(r < chapters[i][j]){
+				book = i + 1;
+				chapter = j + 1;
+				verse = r + 1;
+				return;
+			} else {
+				r -= chapters[i][j];
+			}
+		}
+	}
 }
 
 function genVerseFromBook(){
@@ -133,8 +143,23 @@ function populateLangs(){
 	}
 }
 
+function calculateTotals() {
+	chapterTotal = 0;
+	for(var i = 0;i<chapters.length;i++){ 
+		chapterTotal += chapters[i].length;
+	}
+	
+	verseTotal = 0;
+	for(var i = 0;i<chapters.length;i++){ 
+		for(j = 0; j<chapters[i].length; j++) {
+			verseTotal += chapters[i][j];
+		}
+	}
+}
+
 function bodyLoad(){
 	populateLangs();
+	calculateTotals();
 	document.querySelector("#gen").addEventListener("click", onGenClick);
 	document.querySelector("#langs").addEventListener("change", onLangsChange);
 	document.querySelector("#mode").addEventListener("change", onModeChange);
@@ -142,6 +167,7 @@ function bodyLoad(){
 	onModeChange();
 }
 
+var chapterTotal, verseTotal;
 var defaultLang = "en"
 var chapters = [
 	[50], //Genesis
@@ -158,20 +184,23 @@ var chapters = [
 	[25], //2 Kings
 	[29], //1 Chronicles
 	[36], //2 Chronicles
-	[10], //Ezra
+	[11, 17, 13, 24, 17, 22, 28, 36, 15, 44], //Ezra
 	[13], //Nehemiah
-	[10], //Esther
+	[22, 23, 15, 17, 14, 14, 10, 17, 32, 3], //Esther
 	[42], //Job
 	[150], //Psalms
 	[31], //Proverbs
-	[12], //Ecclesiastes
+	[18, 26, 22, 16, 20, 12, 29, 17, 18, 20, 
+	 10, 14], //Ecclesiastes
 	[17, 17, 11, 16, 16, 13, 13, 14], //Song of Solomon
 	[66], //Isaiah
 	[52], //Jeremiah
 	[22, 22, 66, 22, 22], //Lamentations
 	[48], //Ezekiel
-	[12], //Daniel
-	[14], //Hosea
+	[21, 49, 30, 37, 31, 28, 28, 27, 27, 21, 
+	 45, 13], //Daniel
+	[11, 23, 5, 19, 15, 11, 16, 14, 17, 15, 
+	 12, 14, 16, 9], //Hosea
 	[20, 32, 21], //Joel
 	[15, 16, 15, 13, 27, 14, 17, 14, 15], //Amos
 	[21], //Obadiah
@@ -181,7 +210,8 @@ var chapters = [
 	[17, 20, 19], //Habakkuk
 	[18, 15, 20], //Zephaniah
 	[15, 23], //Haggai
-	[14], //Zechariah
+	[20, 13, 10, 14, 11, 15, 14, 23, 17, 12, 
+	 17, 14, 9, 21], //Zechariah
 	[14, 17, 18, 6], //Malachi
 	[28], //Matthew
 	[16], //Mark
@@ -201,7 +231,8 @@ var chapters = [
 	[18, 26, 17, 22], //2 Timothy
 	[16, 15, 15], //Titus
 	[25], //Philemon
-	[13], //Hebrews
+	[14, 18, 19, 16, 14, 20, 28, 13, 28, 39, 
+	 40, 29, 25], //Hebrews
 	[27, 26, 18, 17, 20], //James
 	[25, 25, 22, 19, 14], //1 Peter
 	[21, 22, 18], //2 Peter
@@ -209,7 +240,9 @@ var chapters = [
 	[13], //2 John
 	[14], //3 John
 	[25], //Jude
-	[22] //Revelation
+	[20, 29, 22, 11, 14, 17, 17, 13, 21, 11, 
+	 19, 17, 18, 20, 7, 21, 18, 24, 21, 15, 
+	 27, 21] //Revelation
 	]
 var languages = [
 	{"lang": "English","option":"en",
